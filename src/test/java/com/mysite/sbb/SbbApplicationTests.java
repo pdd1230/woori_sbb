56e62ed63fd37a6d84657cf3080403868178be3a
pdd1230 <lski@naver.com>
2025-12-16 17:34:11 +0900

    인터페이스 QuestionRepository / AnswerRepository 생성 + JUnit 설치 + 질문생성

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,13 +1,34 @@
 package com.mysite.sbb;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class SbbApplicationTests {
 
+    // DI(의존성 주입) --> questionRepository 라는 객체를 스프링 만들어서 제공해줌
+    @Autowired
+    private QuestionRepository questionRepository;
+
+
     @Test
-    void contextLoads() {
+    void testJpa() {
+
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고 싶습니다.");
+        q1.setCreateDate(LocalDateTime.now()); // static 메서드 호출 --> class명.메서드명
+        this.questionRepository.save(q1);
+
+        Question q2 = new Question();
+        q2.setSubject("스프링부트 모델 질문입니다.");
+        q2.setContent("id는 자동으로 생성되나요?");
+        q2.setCreateDate(LocalDateTime.now()); // static 메서드 호출 --> class명.메서드명
+        this.questionRepository.save(q2);
+
     }
 
 }
