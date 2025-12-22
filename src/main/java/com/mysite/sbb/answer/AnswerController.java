4904372ee7fe6a81336715d607d2fb00da3398c3
pdd1230 <lski@naver.com>
2025-12-22 17:41:37 +0900

    질문 등록기능 추가하기

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -1,10 +1,13 @@
 package com.mysite.sbb.answer;
 
+import com.mysite.sbb.question.QuestionForm;
 import com.mysite.sbb.question.QuestionService;
 import com.mysite.sbb.question.Question;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,15 +19,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AnswerController {
 
     private final QuestionService questionService;
-    private final AnswerService answerService;  // 추가
+    private final AnswerService answerService;
 
-    @PostMapping("/create/{id}")   // {id} 는 질문 Entity의 id 값
+
+
+    @PostMapping("/create/{id}")
     public String createAnswer(Model model,
                                @PathVariable Integer id,
-                               @RequestParam(value="content") String content) {  // textarea --> name ="content"
+                               @Valid AnswerForm answerForm,   // 2줄 추가
+                               BindingResult bindingResult) {
         Question question = this.questionService.getQuestion(id);
-        // TODO: 답변을 저장한다 --> 로직 처리 순서상 해야 할일
-        answerService.createAnswer(question, content); // 답볍을 저장 --> service 호출
+        //if 문으로 bindingResult 에 eror를 체크하여 true이면 question_detail 에서 수정토록 다시 뿌려줌(에러 메세지 같이 보냄)
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+            return "question_detail";
+        }
+        answerService.createAnswer(question, answerForm.getContent()); // content --> answerForm.getContent()로 가져옴
 
         return String.format("redirect:/question/detail/%s", id);
     }
--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,11 +1,12 @@
 package com.mysite.sbb.question;
 
+import com.mysite.sbb.answer.AnswerForm;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,14 +25,30 @@ public class QuestionController {
         return "question_list";
     }
 
-    // URL 매핑 --> @PathVariable
     @GetMapping("/detail/{id}")
     public String detail(Model model,
-                         @PathVariable Integer id) {
+                         @PathVariable Integer id,
+                         AnswerForm answerForm) {
 
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
     }
 
+    @GetMapping("/create")
+//    public String questionCreate() {
+    public String questionCreate(QuestionForm questionForm) { //questionForm --> 양방향(화면-컨트롤) 바인딩
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@Valid QuestionForm questionForm,
+                                 BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+
+        return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
+    }
 }
